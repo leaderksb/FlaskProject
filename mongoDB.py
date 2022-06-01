0bd3af9e5505a240a0d45c5f42dc55ba77e26449
remnantleader <rtleader1228@gmail.com>
2022-06-01 23:31:18 +0900

    server.py index.html login.html signup.html 편집 MongoDB 연동. Bootstrap 적용. 회원가입 기능 구현. 로그인 기능 구현.

---
+++ mongoDB.py
@@ -0,0 +1,78 @@
+from flask import *
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+
+#idChkDictionary = {}  # ID : 해당 ID가 존재하면 1 존재하지 않으면 0
+
+# DB 연동
+conn = MongoClient('mongodb://localhost:27017/')  # MongoDB IP : 127.0.0.1, PORT : 27017, use information
+# DB 생성
+informationDB = conn['information']
+# # collections 생성
+# collect = db.information
+#
+# result = collect.find()
+# for docs in result :
+#     print(docs)
+
+# for docs in db.information.find():
+#     print(docs)
+
+def idChk(id):
+    # # DB 연동
+    # conn = MongoClient('mongodb://localhost:27017/')  # MongoDB IP : 127.0.0.1, PORT : 27017, use information
+    # # DB 생성
+    # informationDB = conn['information']
+    # global idChkDictionary
+    x = informationDB.information.count_documents( {"id":id} )  # 조회된 값 개수
+    # idChkDictionary.update(id, x)
+    # DB 종료
+    # conn.close()
+    return x
+
+def login(id, pw):
+    # 접속 속도 우선순위 : 1) 고객 2) 직원 3) 관리자
+    resultCustomer = informationDB.information.count_documents(  # 조회된 값 개수
+        {"power": "customer", "id": id, "pw": pw}
+    )
+    resultStaff = informationDB.information.count_documents(  # 조회된 값 개수
+        {"power": "staff", "id": id, "pw": pw}
+    )
+    resultManager = informationDB.information.count_documents(  # 조회된 값 개수
+        {"power": "manager", "id": id, "pw": pw}
+    )
+
+    if resultCustomer == 1:
+        return "resultCustomer"
+    elif resultStaff == 1:
+        return "resultStaff"
+    elif resultManager == 1:
+        return "resultManager"
+    else:
+        return "error"
+
+    print(resultCustomer, resultStaff, resultManager)
+
+    # x = informationDB.information.count_documents(  # 조회된 값 개수
+    #     {"id": id, "pw": pw}
+    # )
+    # return x
+
+def signup(name, id, pw, phone, gender, age):
+    # DB 연동
+    # conn = MongoClient('mongodb://localhost:27017/')  # MongoDB IP : 127.0.0.1, PORT : 27017, use information
+    # # DB 생성
+    # informationDB = conn['information']
+    # 삽입된 문서의 ID를 보유 하는 InsertOneResult 객체를 반환. _id 필드를 지정 하지 않으면 MongoDB가 필드를 추가 하고 각 문서에 고유한 ID를 할당.
+    informationDB.information.insert_one( {"power": "customer", "name": name, "id": id, "pw": pw, "phone": phone, "gender": gender, "age": age} )
+    # DB 종료
+    # conn.close(se)
+
+print(idChk("kimsubin"))
+# print(informationDB.information.count_documents( {"id": ""} ))
+# print(type(db.information.find(
+#          {"id": id}
+#      )))
+
+def closeDB():
+    conn.close()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import logging
-import random
+import mongoDB
 
 app = Flask(__name__)
 
@@ -26,7 +26,7 @@ def index():
     # for topic in topics:
     #     liTags = liTags +f'<li><a href="{topic["link"]}">{topic["title"]}</a></li>'
 
-    return render_template("index.html", linkDataHtml=links)
+    return render_template('index.html', linkDataHtml=links)
 
     # return f'''<!doctype html>
     # <html>
@@ -39,24 +39,92 @@ def index():
     # </html>
     # '''
 
-@app.route('/login/')
+@app.route('/login/', methods=['GET','POST'])
 def login():
     print('/login/')
-    return render_template("login.html")
-    # return '/login/'
 
-@app.route('/signup/')
+    if request.method == 'POST':
+        loginIdReceive = request.form.get('loginIdGive')  # 아이디
+        loginPwReceive = request.form.get('loginPwGive')  # 비밀번호
+
+        print("########################################")
+
+        if loginIdReceive == "" or loginIdReceive.strip() == "":  # loginIdReceive에 문자열이 없거나 입력된 문자열이 처음부터 끝까지 공백일 시
+            # loginPwReceive 텍스트 필드에 입력된 문자열이 없으면 팝업창 띄우고 /login/ 페이지로 이동
+
+            print("loginIdReceive == "" or loginIdReceive.strip() == """)
+            return """
+            <script type="text/javascript"> alert("ID를 입력해 주세요."); document.location.href="/login/";</script>
+            """
+        elif loginPwReceive == "" or loginPwReceive.strip() == "":  # loginPwReceive에 문자열이 없거나 입력된 문자열이 처음부터 끝까지 공백일 시
+            # loginPwReceive 텍스트 필드에 입력된 문자열이 없으면 팝업창 띄우고 /login/ 페이지로 이동
+
+            print("loginPwReceive == "" or loginPwReceive.strip() == ")
+            return """
+            <script type="text/javascript"> alert("PW를 입력해 주세요."); document.location.href="/login/";</script>
+            """
+        else:  # loginIdReceive, loginPwReceive 텍스트 필드에 입력된 문자열이 있으면
+            print("로그인 시도 : ")
+            print(mongoDB.login(loginIdReceive, loginPwReceive))
+            if mongoDB.login(loginIdReceive, loginPwReceive) == "resultCustomer":
+                # return render_template('customer.html')
+                return '/customer/<main>/'
+            elif mongoDB.login(loginIdReceive, loginPwReceive) == "resultStaff":
+                return '/staff/<main>/'
+            elif mongoDB.login(loginIdReceive, loginPwReceive) == "resultManager":
+                return '/manager/<main>/'
+            elif mongoDB.login(loginIdReceive, loginPwReceive) == "error":
+                return '/error/<main>/'
+
+    return render_template('login.html')
+
+@app.route('/signup/', methods=['GET','POST'])
 def signup():
     print('/signup/')
-    return render_template("signup.html")
+
+    if request.method == 'POST':
+        signupNameReceive = request.form.get('signupNameGive')  # 이름
+        signupIdReceive = request.form.get('signupIdGive')  # 아이디
+        signupPwReceive = request.form.get('signupPwGive')  # 비밀번호
+        signupPhoneReceive = request.form.get('signupPhoneGive')  # 전화번호
+        signupGenderReceive = request.form.get('signupGenderGive')  # 성별
+        signupAgeReceive = request.form.get('signupAgeGive')  # 나이대
+
+        print("########################################")
+
+        if signupIdReceive == "" or signupIdReceive.strip() == "":  # signupIdReceive에 문자열이 없거나 입력된 문자열이 처음부터 끝까지 공백일 시
+            # signupIdReceive 텍스트 필드에 입력된 문자열이 없으면 팝업창 띄우고 /signup/ 페이지로 이동
+            return """
+            <script type="text/javascript"> alert("ID를 입력해 주세요."); document.location.href="/signup/";</script>
+            """
+        else:  # signupIdReceive 텍스트 필드에 입력된 문자열이 있으면
+            print(mongoDB.idChk(signupIdReceive))
+            # ID 중복 확인
+            if mongoDB.idChk(signupIdReceive) == 1:  # 존재하는 ID라면. 중복된 ID.
+                return """
+                <script type="text/javascript"> alert("이미 사용 중인 ID입니다."), document.location.href="/signup/"; </script>
+                """
+            elif mongoDB.idChk(signupIdReceive) == 0:  # 존재하지 않는 ID라면
+                print("존재하지 않는 ID라면")
+                
+                ###############  여기에 나머지 다른 텍스트 필드 값들이 있는지 확인 코드 추가  ###############
+                
+                mongoDB.signup(signupNameReceive, signupIdReceive, signupPwReceive, signupPhoneReceive, signupGenderReceive, signupAgeReceive)
+                return """
+                <script type="text/javascript"> alert("회원가입이 완료되었습니다."), document.location.href="/login/"; </script>
+                """
+
+        print(signupNameReceive, signupIdReceive, signupPwReceive, signupPhoneReceive, signupGenderReceive, signupAgeReceive)
+
+    return render_template('signup.html')
     # return '/signup/'
 
-@app.route('/manager/<main>/')
+@app.route('/manager/<main>/', methods=['GET','POST'])
 def manager(main):
     print('/manager/<main>/')
     return '/manager/<main>/'
 
-@app.route('/staff/<main>/')
+@app.route('/staff/<main>/', methods=['GET','POST'])
 def requester(main):
     print('/staff/<main>/')
     return '/staff/<main>/'
