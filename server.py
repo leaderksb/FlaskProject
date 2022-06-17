4ee4732240981890f3505c5a2500f2f903c5ee46
remnantleader <rtleader1228@gmail.com>
2022-06-17 15:20:01 +0900

    IP, Port 설정.

--- server.py
+++ server.py
@@ -396,4 +396,4 @@ def customer():
     return render_template('customerProductOrder.html', productexpirydateDataHtml=productexpirydateList)
 
 # app.run(port=5001, debug=True)  # debug 모드로 실행 가능. 실제 서비스 할 때는 사용 X. 편의를 위함.
-app.run(host="3.110.92.255", port=5000, debug=True)  # debug 모드로 실행 가능. 실제 서비스 할 때는 사용 X. 편의를 위함.
+app.run(host="172.31.13.193", port=5000, debug=True)  # debug 모드로 실행 가능. 실제 서비스 할 때는 사용 X. 편의를 위함.
