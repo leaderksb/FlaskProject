cd75aad73ff87faa677f9e207fd66c85475d79b9
remnantleader <rtleader1228@gmail.com>
2022-06-22 17:06:32 +0900

    재배포.

--- server.py
+++ server.py
@@ -267,7 +267,7 @@ def managerProductSaleInquiry():
         print("saleTypeReceive >>", saleTypeReceive)
 
         contentsCnt = mariaDB.productSaleCnt(saleTypeReceive)
-        # contentNames = mariaDB.productSaleNameSelect(saleTypeReceive)
+        contentNames = mariaDB.productSaleNameSelect(saleTypeReceive)
         # contentsDF = mariaDB.productSaleSelect(saleTypeReceive)
         # contentsDF['date'] = pd.to_datetime(contentsDF['date'])  # 데이터프레임 문자열 칼럼 숫자형으로 변환
         # contentsDF['sum_quantity'] = pd.to_numeric(contentsDF['sum_quantity'])  # 데이터프레임 문자열 칼럼 숫자형으로 변환
